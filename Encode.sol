7657c502d5ba9eada2816ec024d53b1d90b855a8
seaona <mariona@gmx.es>
2023-01-27 08:43:19 +0100

    Encoding functions

---
+++ Encode.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+interface IERC20 {
+    function transfer(address, uint) external;
+}
+
+contract Token {
+    function transfer(address, uint) external {
+
+    }
+}
+
+contract AbiEncode {
+    function test(address _contract, bytes calldata data) external {
+        (bool ok,) = _contract.call(data);
+        require(ok, "call failed");
+    }
+
+    function encodeWithSignature(address to, uint amount)
+        external
+        pure
+        returns (bytes memory)
+    {
+        // we can make a mistake with a typo on the func name
+        return abi.encodeWithSignature("transfer(address,uint256)", to, amount);
+    }
+
+    function encodeWithSelector(address to, uint amount)
+        external
+        pure
+        returns (bytes memory)
+    {
+        // we won't make a mistake with a typo on the func name
+        // we can pass incorrect number of params
+        return abi.encodeWithSelector(IERC20.transfer.selector, to, amount);
+    }        
+
+    function encodeCall(address to, uint amount) external pure returns (bytes memory) {
+        // a mistake here won't compile - it's the safest encode
+        return abi.encodeCall(IERC20.transfer, (to, amount));
+    }
+
+}
