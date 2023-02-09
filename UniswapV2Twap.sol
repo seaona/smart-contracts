ca84201969b0a7074a403b5ff2ab6e0c775a994e
seaona <mariona@gmx.es>
2023-02-09 08:04:57 +0100

    Flash loans and Twap

--- UniswapV2Twap.sol
+++ UniswapV2Twap.sol
@@ -1,11 +1,11 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.6.6;
+pragma solidity 0.8.17;
 // NOTE: using solidity 0.6.6 to match imports
 
-import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
-import "@uniswap/lib/contracts/libraries/FixedPoint.sol";
-import "@uniswap/v2-periphery/contracts/libraries/UniswapV2OracleLibrary.sol";
-import "@uniswap/v2-periphery/contracts/libraries/UniswapV2Library.sol";
+import "../node_modules/@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import "../node_modules/@uniswap/lib/contracts/libraries/FixedPoint.sol";
+import "../node_modules/@uniswap/v2-periphery/contracts/libraries/UniswapV2OracleLibrary.sol";
+import "../node_modules/@uniswap/v2-periphery/contracts/libraries/UniswapV2Library.sol";
 
 contract UniswapV2Twap {
     using FixedPoint for *;
