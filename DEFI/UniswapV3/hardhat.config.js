4639ce5f8d79222c4bb492fac2102a8f858d29a0
seaona <mariona@gmx.es>
2023-02-24 09:53:42 +0100

    Uniswap V3 swaps

---
+++ DEFI/UniswapV3/hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.7.6",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://mainnet.infura.io/v3/${process.env.WEB3_INFURA_PROJECT_ID}`
+      }
+    }
+  }
+};
